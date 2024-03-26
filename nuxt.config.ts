4185a2a77eb747f1559b196c1c5c582a1bdd5bfe
Andre Abelmann <3215327+aabelmann@users.noreply.github.com>
2024-03-26 11:38:05 +0100

    Add colors and fonts, including temp ds page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,12 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: [
-    'ress/ress.css',
-    // 'assets/scss/main.scss'
-  ],
+  css: ['ress/ress.css', 'assets/scss/main.scss'],
   modules: ['@pinia/nuxt', '@nuxtjs/google-fonts'],
-  // googleFonts: {
-  //   families: {
-  //     'DM Sans': true,
-  //   },
-  //   download: false,
-  //   useStylesheet: true,
-  // },
+  googleFonts: {
+    families: {
+      'Hanken Grotesk': true,
+      'Schibsted Grotesk': true,
+      'Familjen Grotesk': true,
+    },
+  },
 });
