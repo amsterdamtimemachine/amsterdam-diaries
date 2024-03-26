9a7d6347760189bf2e8ed31c590dc6a4205d9f4a
Andre Abelmann <3215327+aabelmann@users.noreply.github.com>
2024-03-26 10:52:53 +0100

    Setup: Style & Coding

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: [
+    'ress/ress.css',
+    // 'assets/scss/main.scss'
+  ],
+  modules: ['@pinia/nuxt', '@nuxtjs/google-fonts'],
+  // googleFonts: {
+  //   families: {
+  //     'DM Sans': true,
+  //   },
+  //   download: false,
+  //   useStylesheet: true,
+  // },
+  typescript: {
+    shim: true,
+  },
+});
