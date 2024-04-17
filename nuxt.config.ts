62d0d1c940b6b6b9f68d5216289bd06ff82b67ff
Andre Abelmann <andre@totaldesign.com>
2024-04-17 13:41:39 +0000

    Feat: ATM-63 API & Store

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,4 +21,15 @@ export default defineNuxtConfig({
       },
     },
   },
+  runtimeConfig: {
+    app: {
+      getAuthorsUri: 'https://api.lod.uba.uva.nl/queries/ATM/atm-diaries-get-authors/run',
+      getAllDiaries: 'https://api.lod.uba.uva.nl/queries/ATM/atm-diaries-get-all-diaries/run',
+      // + ?entry=entryURL
+      getText: 'https://api.lod.uba.uva.nl/queries/ATM/atm-diaries-get-text/run',
+      // TODO: Optimize this
+      bookBaseUri: 'https://id.amsterdamtimemachine.nl/ark:/81741/amsterdam-diaries/diaries/',
+      entryBaseUri: 'https://id.amsterdamtimemachine.nl/ark:/81741/amsterdam-diaries/annotations/entries/',
+    },
+  },
 });
