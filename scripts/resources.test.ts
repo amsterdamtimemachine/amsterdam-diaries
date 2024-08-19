dd966ce6c84dae687deaf5097000aedcf08d9f04
Rikard Nilsen <rikard@totaldesign.com>
2024-08-19 17:00:04 +0200

    Added resources test

--- scripts/resources.test.ts
+++ scripts/resources.test.ts
@@ -4,7 +4,32 @@ import { importResources } from './resources';
 describe('resources', async () => {
   it('Can parse', async () => {
     const result = await importResources('http://localhost:3000/testdata/resources.jsonld');
-    // TODO: Fix this test
-    expect(result).toEqual([{}]);
+    expect(result).toEqual({
+      people: [
+        {
+          id: 'http://www.wikidata.org/entity/Q104369',
+          name: 'Ernst Laqueur',
+          description: 'Nederlands hoogleraar Farmacologie',
+        },
+      ],
+      organizations: [
+        {
+          id: 'http://www.wikidata.org/entity/Q172579',
+          name: 'Koninkrijk Italië',
+          description: '1861–1946',
+          latitude: 41.9,
+          longitude: 12.5,
+        },
+      ],
+      places: [
+        {
+          id: 'http://www.wikidata.org/entity/Q10041',
+          name: 'Soest',
+          description: 'gemeente in de provincie Utrecht, Nederland',
+          latitude: 52.183333,
+          longitude: 5.283333,
+        },
+      ],
+    });
   });
 });
