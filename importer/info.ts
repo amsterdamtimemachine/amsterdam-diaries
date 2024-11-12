207c4b2a165cc1e4ab08929aae33e150b9dea8bf
Andre Abelmann <3215327+aabelmann@users.noreply.github.com>
2024-11-12 11:34:45 +0100

    Feat: ATM-226 (#9)
    * Fix ' in the package.json
    
    * Start using jsonld for the info data

--- importer/info.ts
+++ importer/info.ts
@@ -1,3 +1,5 @@
+import jsonld from 'jsonld';
+
 const definitionInfo = {
   name: 'info',
   fields: [
@@ -17,16 +19,35 @@ const definitionInfo = {
   ],
 };
 
+/**
+ * Framing context
+ */
+const frame = {
+  '@context': {
+    '@vocab': 'https://schema.org/',
+    id: '@id',
+    type: '@type',
+    title: 'http://schema.org/name',
+    description: 'http://schema.org/description',
+  },
+  id: {},
+  type: {},
+  title: {},
+  description: {},
+};
+
 const importInfo = async (importUrl: string): Promise<ParsedResponse> => {
   const result = await fetch(importUrl);
-  const json: Record<string, InfoData> = await result.json();
-  return Object.keys(json).reduce(
-    (acc: ParsedResponse, id) => {
-      acc.info!.push({
-        id,
-        title: json[id].title,
-        description: json[id].description,
-      });
+  const json = await result.json();
+  // TODO: Remove this line once the context has been altered in the jsonld file
+  json['@context'] = ['https://schema.org/'];
+  const framed = await jsonld.frame(json, frame, { omitGraph: false });
+  const items = (framed['@graph'] ?? []) as RawInfo[];
+
+  return items.reduce(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    (acc: ParsedResponse, { type, ...item }: RawInfo) => {
+      acc.info!.push(item);
       return acc;
     },
     {
